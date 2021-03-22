8ab9c632e72c3e754f5837a58baebfc2f4e6d32e
BaezCrdrm <dev.baez_crdrm@outlook.com>
2021-03-22 14:01:55 -0600

    Clientes controller inicial

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MSClientes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSClientes.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        [HttpGet("buscar")]
+        public async Task<ActionResult<Cliente>> Search()
+        {
+            return Ok();
+        }
+    }
+}
