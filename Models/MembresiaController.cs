6b81f0402da6dbf87957b054461affa62e20f4a8
jonathan <jhoni65475@gmail.com>
2021-03-22 20:22:44 -0600

    tecnicamente, debe funcionar

---
+++ Models/MembresiaController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSClientes.Models
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MembresiaController : ControllerBase
+    {
+        clientesContext bd;
+
+
+        private readonly ILogger<MembresiaController> _logger;
+
+        public MembresiaController(ILogger<MembresiaController> logger)
+        {
+            _logger = logger;
+            bd = new clientesContext();
+        }
+
+        [HttpGet("buscar")]
+        public async Task<ActionResult<Membresia>> Get([FromQuery] int idCliente = -1, [FromQuery] int tipo = -1)
+        {
+            List<Membresia> membresia = null;
+
+            membresia = await bd.Membresias
+                .Where(p => (p.IdCliente >= 0 && p.IdCliente == idCliente))
+                .Where(p => (p.Tipo >= 0 && p.Tipo == tipo))
+                .ToListAsync();
+            if (membresia == null) return BadRequest();
+            else return Ok(membresia);
+        }
+    }
+}
