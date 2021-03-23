e4d3a2a98eab0105dde9316c33e970364147dcad
Pklove3459 <61339804+Pklove3459@users.noreply.github.com>
2021-03-23 11:45:59 -0600

    RegistroCliente
    Se agrego la funcion "RegistrarCliente"
    Las variables dbContext y un log

--- Controllers/ClientesController.cs
+++ Controllers/ClientesController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using MSClientes.Models;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MSClientes.Models;
 
 namespace MSClientes.Controllers
 {
@@ -12,10 +14,37 @@ namespace MSClientes.Controllers
     [ApiController]
     public class ClientesController : ControllerBase
     {
+        clientesContext dbContext;
+        private readonly ILogger<ClientesController> log;
+
         [HttpGet("buscar")]
         public async Task<ActionResult<Cliente>> Search()
         {
             return Ok();
+        }
+
+         [HttpPost("RegistrarCliente")]
+        public async Task<ActionResult<Cliente>> Add([FromBody]Cliente cliente)
+        {
+            if(cliente == null)
+            {
+                log.LogError("Ingreso datos vacios");
+                return BadRequest("Ingreso datos vacios");
+            }
+
+            try
+            {
+                dbContext.Entry(cliente).State = EntityState.Added;
+                await dbContext.SaveChangesAsync();
+
+                log.LogInformation("Se agregó nuevo cliente: {0}", cliente.Nombre);
+                return Created("", cliente);
+            }
+            catch (Exception ex)
+            {
+                log.LogError("Sucedio una excepcion:\n" + ex.Message);
+                return BadRequest(ex);
+            }
         }
     }
 }
