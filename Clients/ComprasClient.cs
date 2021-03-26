d34334a948d4be1a07567dfb0d998df1819f3f50
Pklove3459 <61339804+Pklove3459@users.noreply.github.com>
2021-03-26 01:47:34 -0600

    Creacion de metodos de busqueda y detalle

--- Clients/ComprasClient.cs
+++ Clients/ComprasClient.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using System.Net.Http;
 using System.Threading.Tasks;
 using MSInventario.Models;
+using MSCompras.Models;
 
 namespace GatewayTienda.Clients
 {
@@ -17,9 +18,50 @@ namespace GatewayTienda.Clients
             client = new HttpClient();
         }
 
-        // public async Task<Compra[]> BuscaCompra(DateTime inicio, DateTime fin)
-        // {
-            
-        // }
+        public async Task<Compra[]> BusquedaCompra(int idCompra = -1)
+        {
+            Compra[] listaCompras;
+            string url = urlServicio + "/compras/buscar?";
+
+            if(idCompra >=0) 
+            {
+                url += "idCompra=" + idCompra;
+            }
+            try
+            {
+                string datosCompra = await client.GetStringAsync(url);
+                listaCompras = JsonConvert.DeserializeObject<Compra[]>(datosCompra);
+                return listaCompras;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nError al obtener una respuesta.");
+                Console.WriteLine("Error: {0}", ex.Message);
+                return null;
+            }
+        }
+
+        public async Task<Compra> DetallesCompra(int idCompra)
+        {
+            Compra compraDetallada;
+            string url = urlServicio + "/compras/detalle/";
+
+            if(idCompra >= 0)
+            {
+                urlServicio += idCompra;
+            }
+            try
+            {
+                string compraDetalle = await client.GetStringAsync(url);
+                compraDetallada = JsonConvert.DeserializeObject<Compra>(compraDetalle);
+                return compraDetallada;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("\nError al obtener una respuesta.");
+                Console.WriteLine("Error: {0}", ex.Message);
+                return null;
+            }
+        }
     }
 }
